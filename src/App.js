08e675ab1a1a690d1f74641bec4dd1aea7083e44
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-08-28 03:51:28 +0530

    browser router configured

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import SignUp from "./components/pages/signUp/signUp";
+import Login from "./components/pages/login/login";
+import ResetPassword from "./components/pages/resetPassword/resetPassword";
+import Home from "./components/pages/home/home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
- 
+      <BrowserRouter>
+          <Routes >
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signUp" element={<SignUp/>} />
+            <Route path="/resetPassword" element={<ResetPassword/>} />
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
