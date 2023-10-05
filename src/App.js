65161cbdc585e5e29d7af1e78fcad2eb7f6dc4f1
aknair95 <aakashunair01031995@gmail.com>
2023-10-06 04:41:33 +0530

    material ui & icon installed

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Login from "./components/pages/login/login";
 import ResetPassword from "./components/pages/resetPassword/resetPassword";
 import Home from "./components/pages/home/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavigationBar from "./components/navigationBar";
+import Sidebar from "./components/sideBar";
+import './index.css';
+import ComposeMail from "./components/pages/composeMail/composeMail";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <BrowserRouter>
-          <Routes >
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/signUp" element={<SignUp/>} />
-            <Route path="/resetPassword" element={<ResetPassword/>} />
-          </Routes>
+          <NavigationBar/>
+          <div className="app_body">
+            <Sidebar/>
+            <Routes >
+              <Route path="/" element={<Home/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/signUp" element={<SignUp/>}/>
+              <Route path="/forgotPswd" element={<ResetPassword/>}/>
+              <Route path="/compose" element={<ComposeMail/>}/>
+            </Routes>
+          </div>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
