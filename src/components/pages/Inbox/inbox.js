482a5c0d449531de61ebc7d72e81046e2ee35e10
aknair95 <aakashunair01031995@gmail.com>
2023-11-10 03:54:10 +0530

    Inbox GET request from firebase to fetch mails

--- src/components/pages/Inbox/inbox.js
+++ src/components/pages/Inbox/inbox.js
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { Alert } from "@mui/material";
 
 const Inbox=() =>{
-    return(
-        <>
+    const getInboxMails=() =>{
+        try{
+            const response=axios.get(`https://mail-box-client-f2b69-default-rtdb.firebaseio.com/inbox.json`);
+        } catch(error){
+            <Alert severity="danger">!!! Mail Fetching Failed !!!</Alert>
+        }
+    }
 
-        </>
+    useEffect(()=>{
+        getInboxMails();
+    },[]);
+
+    return(
+        <ul>
+
+        </ul>
     )
 }
 
