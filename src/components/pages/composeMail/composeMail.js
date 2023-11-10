d2c9b5706443589aa54f0c61036168aa5431ec16
aknair95 <aakashunair01031995@gmail.com>
2023-11-11 03:02:54 +0530

    editor content fetch and converted to plain text

--- src/components/pages/composeMail/composeMail.js
+++ src/components/pages/composeMail/composeMail.js
@@ -1,31 +1,80 @@
 import { Button, Form, InputGroup } from "react-bootstrap";
 import classes from "./compose.module.css";
 import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { useRef, useState } from "react";
+import { Alert } from "@mui/material";
+import axios from "axios";
 
 const ComposeMail=() =>{
-    const sendMailHandler=() =>{
-        alert("!! Mail sent successfully !!")
+    const [editorState,setEditorState]=useState(() =>EditorState.createEmpty());
+
+    const emailRef=useRef();
+    const subjectRef=useRef();
+
+    const outboxFirebase= async (updatedEmail,sentMailData) =>{
+        try{
+            await axios.post(`https://mail-box-client-f2b69-default-rtdb.firebaseio.com/inbox/${updatedEmail}.json`,{
+                email: sentMailData.email,
+                subject: sentMailData.subject,
+                content: sentMailData.content
+            });
+            <Alert severity="success">Sent</Alert>  
+        } catch(error){
+            <Alert severity="danger">!!! Error !!!</Alert>
+        }
     }
+
+    const sendMailHandler=(e) =>{
+        e.preventDefault();
+        const contentState=editorState.getCurrentContent();
+        const rawContentState=convertToRaw(contentState);
+        const plainContent=rawContentState.blocks[0].text;
+     
+        const enteredEmail=emailRef.current.value;
+        const enteredSubject=subjectRef.current.value;
+
+        const email1=enteredEmail.replace("@","");
+        const updatedEmail=email1.replace(".","");
+
+        const sentMailData={
+            email: enteredEmail,
+            subject: enteredSubject,
+            content: plainContent 
+        }
+        outboxFirebase(updatedEmail,sentMailData);
+        emailRef.current.value="";
+        subjectRef.current.value="";
+        setEditorState("");
+    }
+
     return(
         <Form className={classes.formContainer}>
             <InputGroup className="mb-3">
                 <InputGroup.Text>To</InputGroup.Text>
-                <Form.Control 
+                <Form.Control
+                 type="email" 
                  placeholder="example@gmail.com" 
                  required 
-                 size="md"/>
+                 size="md"
+                 ref={emailRef}/>
             </InputGroup>
             <InputGroup className="mb-3">
                 <InputGroup.Text>Subject</InputGroup.Text>
-                <Form.Control 
+                <Form.Control
+                 type="text" 
                  required
-                 size="md"/>
+                 size="md"
+                 ref={subjectRef}/>
             </InputGroup>
-            <div className={classes.editorSpace}>
-                <Editor/>
-            </div>
-            <Button onClick={sendMailHandler}>Send</Button>
+            <Editor 
+                toolbarClassName={classes.toolbar}
+                editorClassName={classes.editorSpace}
+                wrapperClassName={classes.wrapper}
+                editorState={editorState}
+                onEditorStateChange={setEditorState} />
+            <Button type="button" onClick={sendMailHandler}>Send</Button>
         </Form>
     )
 }
