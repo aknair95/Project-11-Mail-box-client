02c63a658acdfbb06d9e8615cbc10d6120d99045
aknair95 <aakashunair01031995@gmail.com>
2023-11-15 04:17:51 +0530

    mail open component created

---
+++ src/components/mailOpen.js
@@ -0,0 +1,15 @@
+
+const MailOpen=() =>{
+    return(
+        <>
+            <h5>From-</h5>
+            <h5>To-</h5>
+            <br/><br/>
+            <h5>Subject-</h5>
+            <br/><br/>
+            <p></p>
+        </>
+    )
+}
+
+export default MailOpen;
