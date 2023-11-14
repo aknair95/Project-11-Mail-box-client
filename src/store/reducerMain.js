81e88f8e221dafad3c62d73f560fb7e069b0e30d
aknair95 <aakashunair01031995@gmail.com>
2023-11-15 03:43:45 +0530

    reducer configured

--- src/store/reducerMain.js
+++ src/store/reducerMain.js
@@ -1,9 +1,10 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authReducer";
+import mailReducer from "./mailReducer";
 
 const storeReducer= configureStore({
-    reducer: { authentication: authReducer.reducer }
+    reducer: { authentication: authReducer.reducer, mailDetails: mailReducer }
 });
 
 export default storeReducer;
