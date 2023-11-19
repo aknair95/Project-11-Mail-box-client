5b0e4b3d10df097e155c66a75d9f2d44bcd18873
aknair95 <aakashunair01031995@gmail.com>
2023-11-20 00:36:16 +0530

    delete mail logic added

--- src/store/mailReducer.js
+++ src/store/mailReducer.js
@@ -1,44 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Alert } from "@mui/material";
 
-const initialState={ inboxMailList:[], outboxMailList:[] }
+
+    const getAllMails= async() =>{
+      try{
+          const response=await axios.get(`https://mail-box-client-f2b69-default-rtdb.firebaseio.com/mails.json`);
+          return response.data;
+          
+        //   if(response.data!==null){
+        //     dispatch(mailActions.addMail(response.data));
+        //   }
+      } catch(error){
+          <Alert severity="danger">!!! Mail Fetching Failed !!!</Alert>
+      }
+    }
+   
+const initialState={ allMails: getAllMails(), outboxMails:[] }
 
 const mailReducer=createSlice({
     name: "mailDetails",
     initialState: initialState,
     reducers: {
-        addInboxMail(state,action){
-            state.inboxMailList=[...state.inboxMailList,action.payload];
+        addMail(state,action){
+            state.allMails.push({...action.payload});
         },
-        deleteInboxMail(state,action){
-            state.inboxMailList=state.inboxMailList.filter((mail) =>{
-                return action.payload!==mail.id;
-            });
-        },
-        deleteOutboxMail(state,action){
-            state.outboxMailList=state.outboxMailList.filter((mail) =>{
-                return action.payload!==mail.id;
-            });
-        },
-        markMailRead(state,action){
-            state.inboxMailList=state.inboxMailList.map((mail) =>{
-                if(mail.id===action.payload){
-                    mail.read=true;
-                }
-                return mail;
-            });
-        },
-        unreadMail(state){
-            const unreadCount=state.inboxMailList.reduce((count,mail) =>{
-                if(mail.read===false){
-                    return count++;
-                }
-                return count;
-            },0);
-            return unreadCount;
+        deleteMail(state,action){
+            state.allMails.filter((mail) =>{ return mail.id!==action.payload.id});
         }
-    }
-})
+        }
+    })
 
-export const mailActions=mailReducer.reducer;
+export const mailActions=mailReducer.actions;
 
 export default mailReducer;
