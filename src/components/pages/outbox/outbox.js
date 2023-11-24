836e25df197febfd25e99550ffd91fc229cc7db7
aknair95 <aakashunair01031995@gmail.com>
2023-11-24 20:41:12 +0530

    outbox component for viewing sent mails

--- src/components/pages/outbox/outbox.js
+++ src/components/pages/outbox/outbox.js
@@ -1,8 +1,38 @@
 
+import { Button, ListGroup } from "react-bootstrap";
+import classes from "./outbox.module.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { mailActions } from "../../../store/mailReducer";
+
 const Outbox=() =>{
+    const dispatch=useDispatch();
+    
+    const navigate=useNavigate();
+    const allmails=useSelector((state) => state.mailDetails.allMails);
+
+    const onClickMailOpenHandler=(e) =>{
+        navigate("/inbox/mailOpen");
+    }
+
+    const onDeleteMailHandler=(e) =>{
+        dispatch(mailActions.deleteMail(e.mail.id));
+    }
+
     return(
         <>
-
+           { 
+            allmails.map((mail) =>{
+                return( 
+                        <ListGroup as="ul" className={classes.mailList}>      
+                            <ListGroup.Item as="li" variant="primary" onClick={onClickMailOpenHandler} key={mail.id} mail={mail}>
+                                <button type="checkbox" />
+                                <p>{mail.receiver}</p><p>{mail.subject}</p><p>{mail.content}</p>
+                                <Button type="click" variant="danger" onClick={onDeleteMailHandler}>Delete</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    )
+            })}
         </>
     )
 }
