d95886d75de7526f969508dff6e2f43b564392c2
aknair95 <aakashunair01031995@gmail.com>
2023-12-15 22:55:21 +0530

    extra feature unread msg badge added

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -2,13 +2,29 @@ import classes from "./sidebar.module.css";
 import MailIcon from '@mui/icons-material/Mail';
 import OutboxIcon from '@mui/icons-material/Outbox';
 import CreateIcon from '@mui/icons-material/Create';
+import Badge from '@mui/material/Badge';
+import { useSelector } from "react-redux";
 
 const Sidebar=() =>{
+    const allmails=useSelector((state) => state.mailDetails.allMails);
+    const userEmail=localStorage.getItem("email");
+    const token=localStorage.getItem("token");
+
+    let receivedMails=allmails.filter((mail) =>{
+        return mail.receiver===userEmail;
+    });
+
+    const unreadCount=receivedMails.reduce((count,mail) =>{
+        if(mail.read===false){
+            count++;
+        }
+        return count;
+    },0);
 
     const sideBarData=[
         {
             title:" Inbox",
-            icon: <MailIcon/>,
+            icon: <Badge color="secondary" badgeContent={unreadCount} showZero invisible={token===null}><MailIcon/></Badge>  ,
             path: "/inbox"
         },
         {
